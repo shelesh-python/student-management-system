44297d393613094689ca2f1abf575d37033bccd2
shelesh-python <sheleshc87@gmail.com>
2026-01-02 02:03:30 +0530

    Fix apps config and remove startup DB access

--- student_management/settings.py
+++ student_management/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',  
     'rest_framework.authtoken',  
-    'students.apps.StudentsConfig',
+   'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
--- students/apps.py
+++ students/apps.py
@@ -1,15 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
 class StudentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'students'
-
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@gmail.com",
-                password="admin123"
-            )
